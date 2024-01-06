ee8cba2fcad96592b8633ad3127496856fe361a4
Obua Stella <stellaobua123@gmail.com>
2024-01-06 17:22:23 +0100

    Installed react icons

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,13 @@
+import Navbar from "./components/Navbar";
 import "./index.css";
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-blue-700 text-center">
-        the4Coders
-      </h1>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`sm:px-16 px-6 flex justify-center items-center`}>
+        <div className={`xl:max-w-[1280px] w-full`}>
+          <Navbar />
+        </div>
+      </div>
     </div>
   );
 }
