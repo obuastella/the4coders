f7a9a58c873285bb9facb99581b9e0e18c2ea9ff
Obua Stella <stellaobua123@gmail.com>
2024-01-06 17:40:14 +0100

    WIP: Working on Navbar Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,8 @@ import Navbar from "./components/Navbar";
 import "./index.css";
 function App() {
   return (
-    <div className="bg-primary w-full overflow-hidden">
-      <div className={`sm:px-16 px-6 flex justify-center items-center`}>
-        <div className={`xl:max-w-[1280px] w-full`}>
-          <Navbar />
-        </div>
-      </div>
+    <div className="">
+      <Navbar />
     </div>
   );
 }
