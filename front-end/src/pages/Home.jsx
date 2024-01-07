826b675486e234f834143ae884e3e50fe75682cb
Obua Stella <stellaobua123@gmail.com>
2024-01-07 23:10:37 +0100

    WIP:hero

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <div className=" h-screen">
-      <section className="hero-section  w-[90%] m-auto h-5/6 flex flex-col md:flex-row md:flex-wrap">
-        <div className="hero-text mt-14 w-full md:w-2/4 flex flex-col justify-center gap-10">
+      <section className="hero-section w-[90%] m-auto border-2 border-red-500 flex flex-col md:flex-row md:flex-wrap">
+        <div className="hero-text mt-14 w-full md:w-2/4 flex flex-col justify-center gap-10 border-2 border-green-500">
           <h1 className="text-3xl font-semi-bold sm:text-5xl md:text-5xl lg-7xl">
             Great <span className="font-bold text-primaryColor">Product</span>{" "}
             is built by great{" "}
@@ -16,7 +16,7 @@ function Home() {
             Let's get started!
           </button>
         </div>
-        <div className="hero-body flex justify-center items-center w-full md:w-2/4 max-h-5/6 mt-14">
+        <div className="hero-body flex justify-center items-center w-full md:w-2/4 max-h-5/6 mt-14 border-2 border-blue-200">
           <img className="" src="assets/images/hero-img.png" alt="team" />
         </div>
       </section>
