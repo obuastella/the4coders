e4edb06324c94c38ddca6ee7bd4b35816a2479f3
Obua Stella <stellaobua123@gmail.com>
2024-01-07 15:52:23 +0100

    WIP: HERO-SECTION

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <div className=" h-screen">Home Page</div>;
+  return (
+    <div className=" h-screen">
+      <section className="hero-section border-2 border-red-500 w-full h-5/6 flex">
+        <div className="hero-text border-2 border-blue-600 h-5/6 w-2/4"></div>
+        <div className="hero-body border-2 border-green-600 h-5/6 w-2/4"></div>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
