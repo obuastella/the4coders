3a1c8ed8625b19fac590d71c0130bcda85bff420
Obua Stella <stellaobua123@gmail.com>
2024-01-09 20:30:53 +0100

    Created an array of card items

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -30,4 +30,16 @@ const data = [
     review:
       "A website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
   },
+  {
+    name: "Software Testing Services",
+    img: "assets/images/card1.png",
+    review:
+      "A website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
+  },
+  {
+    name: "Mobile App Development",
+    img: "assets/images/card1.png",
+    review:
+      "A website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
+  },
 ];
