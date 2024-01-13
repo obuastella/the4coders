f02f161d51100939d80ddc3d02ee2e944d134098
Obua Stella <stellaobua123@gmail.com>
2024-01-13 23:35:44 +0100

    Added section img

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -35,7 +35,7 @@ function Home() {
         </div>
       </section>
       {/* Leading Companies */}
-      <section className="w-[90%] h-5/6 m-auto border-2 border-red-400 flex ">
+      <section className="w-[90%] h-5/6 m-auto border-2 border-red-400 flex flex-wrap">
         <div className="border-2 border-orange-200 h-5/6 w-1/2"></div>
         <div className="border-2 border-blue-200 h-5/6 w-1/2"></div>
       </section>
