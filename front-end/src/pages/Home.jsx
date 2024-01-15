e8a2b3b4945ea66a4c33df6c4ab799e113ff77ac
Obua Stella <stellaobua123@gmail.com>
2024-01-15 22:28:18 +0100

    Centered items

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -36,7 +36,7 @@ function Home() {
       </section>
       {/* Leading Companies */}
       <section className="w-[90%] h-5/6 m-auto border-2 border-red-400 flex flex-wrap">
-        <div className="border-2 border-orange-200 h-5/6 w-1/2 m-auto">
+        <div className="border-2 border-orange-200 h-5/6 w-1/2 flex flex-col  justify-center">
           <h2 className="text-5xl">
             Leading Companies trust us{" "}
             <span className="font-bold"> to develop Software</span>
@@ -51,7 +51,7 @@ function Home() {
             software development company that guarantees the successful delivery
             of your project.
           </p>
-          <button className="text-primaryColor">
+          <button className="text-primaryColor me-0 w-2/4">
             See more Information &rarr;
           </button>
         </div>
