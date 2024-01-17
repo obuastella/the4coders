c56c80c5a4bc213bbb700e2063f24c6f3b573f5a
Obua Stella <stellaobua123@gmail.com>
2024-01-17 22:30:40 +0100

    Finalized section

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -61,7 +61,12 @@ function Home() {
           alt="teams"
         />
       </section>
-      <section className="border-2 border-pink-900 h-2/3 w-[90%] m-auto"></section>
+      <section className="border-2 border-pink-900 w-[90%] m-auto mt-[13%]">
+        <h2 className="text-3xl">
+          Meet the people <br />
+          <span className="font-bold"> We are Working With </span>
+        </h2>
+      </section>
     </div>
   );
 }
