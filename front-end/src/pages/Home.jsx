bf3ceb9f6253c5ecd6ccf825369671136c79727d
Obua Stella <stellaobua123@gmail.com>
2024-01-23 04:37:49 +0100

    WIP

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -87,7 +87,10 @@ function Home() {
         </p>
         <div className="mt-12 w-full h-40 border-2 border-red-700">
           {/* This should be broken down into a component and mapped through */}
-          <div className="w-24 h-24 shrink-0 grow-0 rounded-full bg-green-300 text-green-700"></div>
+          <div className="w-24 h-24 shrink-0 grow-0 rounded-full bg-green-300 text-green-700 mb-2"></div>
+          <div className="w-24 border-2">{/* Star Icons here */}</div>
+          <div className="text-primaryColor font-semibold">Name here</div>
+          <div className="">Occupation here</div>
         </div>
       </section>
     </div>
