ff45db33d082badacd5df7ca20f735f8afcd935b
Obua Stella <stellaobua123@gmail.com>
2024-02-15 23:26:10 +0100

    Updated component structure

--- front-end/src/components/Profile.jsx
+++ front-end/src/components/Profile.jsx
@@ -1,6 +1,7 @@
+import React from "react";
 export const Profile = () => {
   return (
-    <>
+    <div className="mt-12 w-full border-2 border-blue-700">
       <img
         className="w-32 h-32 shrink-0 grow-0"
         src="assets/images/customer1.png"
@@ -9,7 +10,7 @@ export const Profile = () => {
       <div className="w-24 border-2">{/* Star Icons here */}</div>
       <div className="text-primaryColor font-semibold">Romeena De silva</div>
       <div className="">Lecturer</div>
-    </>
+    </div>
   );
 };
 // Multiple cards will be added so either make it a list and loop through it or use props to pass it to the parent.
