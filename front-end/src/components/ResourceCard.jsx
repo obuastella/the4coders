d6f334ab7326e13bf07ef0e325cad7cb2c5a7af1
Obua Stella <stellaobua123@gmail.com>
2024-03-03 16:40:22 +0100

    looped resource card to an array

--- front-end/src/components/ResourceCard.jsx
+++ front-end/src/components/ResourceCard.jsx
@@ -1,19 +1,40 @@
 function ResourceCard() {
+  const resourceData = [
+    {
+      img: "./assets/images/resource-img2.png",
+      text: " How to build a Scalable Application up to 1 million users on AWS blah blah",
+    },
+    {
+      img: "./assets/images/resource-img2.png",
+      text: " How to build a Scalable Application up to 1 million users on AWS blah blah",
+    },
+    {
+      img: "./assets/images/resource-img2.png",
+      text: " How to build a Scalable Application up to 1 million users on AWS blah blah",
+    },
+    {
+      img: "./assets/images/resource-img2.png",
+      text: " How to build a Scalable Application up to 1 million users on AWS blah blah",
+    },
+    {
+      img: "./assets/images/resource-img2.png",
+      text: " How to build a Scalable Application up to 1 million users on AWS blah blah",
+    },
+  ];
+
   return (
     <div className="my-8 flex justify-evenly flex-wrap gap-4">
-      <div className="w-50 h-50">
-        <img
-          className="w-50 h-50"
-          src="./assets/images/resource-img2.png"
-          alt="laptop-img"
-        />
-        <p className=" max-w-64">
-          How to build a Scalable Application up to 1 million users on AWS
-        </p>
-        <button className=" float-right">Read more &rarr;</button>
-      </div>
+      {resourceData.map((data) => (
+        <>
+          <div className="w-50 h-50" key={data.text}>
+            <img className="w-50 h-50" src={data.img} alt="laptop-img" />
+            <p className=" max-w-64">{data.text}</p>
+            <button className=" float-right">Read more &rarr;</button>
+          </div>
+        </>
+      ))}
     </div>
   );
 }
-
+// To do add key value
 export default ResourceCard;
