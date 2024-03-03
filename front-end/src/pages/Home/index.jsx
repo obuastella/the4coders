8a120e1223f19ad42364cb012a7b77a3cb567996
Obua Stella <stellaobua123@gmail.com>
2024-03-03 16:20:11 +0100

    Added resource card

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -125,12 +125,18 @@ function Home() {
           Featured <br />
           <span className="font-bold">Resources</span>
         </h2>
-        <div className="resources-card">
-          <div className="w-[40%] h-[40%] border-2"></div>
-          <p>
-            How to build a Scalable Application up to 1 million users on AWS
-          </p>
-          <button>Read more &gt;</button>
+        <div className="my-8 flex justify-evenly flex-wrap gap-4">
+          <div className="w-50 h-50">
+            <img
+              className="w-50 h-50"
+              src="./assets/images/resource-img2.png"
+              alt="laptop-img"
+            />
+            <p className=" max-w-64">
+              How to build a Scalable Application up to 1 million users on AWS
+            </p>
+            <button className=" float-right">Read more &rarr;</button>
+          </div>
         </div>
       </section>
     </>
